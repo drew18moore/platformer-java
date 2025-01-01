9d29d565f4dce69ad2a97f40891c1e86c810a326
Drew <dcm752052@gmail.com>
2024-12-31 20:00:07 -0500

    Moved movement sprite animation logic into Entity class.

--- src/main/java/Entity.java
+++ src/main/java/Entity.java
@@ -5,26 +5,35 @@ public class Entity {
     public int screenX;
     public int screenY;
 
+    public int spriteWidth;
+    public int spriteHeight;
+
     public float velocityY = 0;
 
     public boolean isSolid = false;
     public boolean useGravity = false;
 
-    BufferedImage sprite;
+    BufferedImage[] movementSprites;
+    public int spriteCounter = 0;
+    public int spriteNum = 0;
+    public boolean facingLeft = false;
 
-    public Entity(int screenX, int screenY, BufferedImage sprite) {
+    public Entity(int screenX, int screenY, BufferedImage[] movementSprites, int spriteWidth, int spriteHeight) {
         this.screenX = screenX;
         this.screenY = screenY;
 
-        if (sprite != null) {
-            this.sprite = sprite;
+        this.spriteWidth = spriteWidth;
+        this.spriteHeight = spriteHeight;
+
+        if (movementSprites != null) {
+            this.movementSprites = movementSprites;
         } else {
             BufferedImage placeholder = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
             Graphics2D g2 = placeholder.createGraphics();
             g2.setColor(Color.BLUE);
             g2.fillRect(0, 0, 50, 50);
             g2.dispose();
-            this.sprite = placeholder;
+            this.movementSprites = new BufferedImage[]{placeholder};
         }
     }
 
@@ -38,7 +47,11 @@ public class Entity {
     }
 
     public void draw(Graphics2D g2) {
-        g2.drawImage(this.sprite, this.screenX, this.screenY, Constants.TILE_SIZE, Constants.TILE_SIZE, null);
+        if (facingLeft) {
+            g2.drawImage(this.movementSprites[spriteNum], this.screenX + this.spriteWidth * Constants.SCALE, this.screenY, this.spriteWidth * -Constants.SCALE, this.spriteHeight * Constants.SCALE, null);
+        } else {
+            g2.drawImage(this.movementSprites[spriteNum], this.screenX, this.screenY, this.spriteWidth * Constants.SCALE, this.spriteHeight * Constants.SCALE, null);
+        }
     }
 
     public Rectangle getBounds() {
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -5,25 +5,19 @@ import java.io.IOException;
 
 public class Player extends Entity {
     private final Input keyListener;
-    public BufferedImage[] movementSprites;
-    public int spriteCounter = 0;
-    public int spriteNum = 0;
-    public boolean facingLeft = false;
 
     public Player(int screenX, int screenY, final Input input) {
-        super(screenX, screenY, null);
+        super(screenX, screenY, null, Constants.PLAYER_SPRITE_TILE_SIZE, Constants.PLAYER_SPRITE_TILE_SIZE);
         this.useGravity = true;
         this.keyListener = input;
 
         try {
             BufferedImage spriteSheet = ImageIO.read(getClass().getResourceAsStream("/sprites/knight.png"));
 
-            movementSprites = new BufferedImage[8];
+            this.movementSprites = new BufferedImage[8];
             for (int i = 0; i < 8; i++) {
                 movementSprites[i] = spriteSheet.getSubimage(i * Constants.PLAYER_SPRITE_TILE_SIZE, 2 * Constants.PLAYER_SPRITE_TILE_SIZE, Constants.PLAYER_SPRITE_TILE_SIZE, Constants.PLAYER_SPRITE_TILE_SIZE);
             }
-
-            this.sprite = movementSprites[spriteNum];
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -59,12 +53,6 @@ public class Player extends Entity {
     }
 
     public void draw(Graphics2D g2) {
-        this.sprite = movementSprites[spriteNum];
-
-        if (facingLeft) {
-            g2.drawImage(this.sprite, this.screenX + Constants.PLAYER_SPRITE_TILE_SIZE * Constants.SCALE, this.screenY, Constants.PLAYER_SPRITE_TILE_SIZE * -Constants.SCALE, Constants.PLAYER_SPRITE_TILE_SIZE * Constants.SCALE, null);
-        } else {
-            g2.drawImage(this.sprite, this.screenX, this.screenY, Constants.PLAYER_SPRITE_TILE_SIZE * Constants.SCALE, Constants.PLAYER_SPRITE_TILE_SIZE * Constants.SCALE, null);
-        }
+        super.draw(g2);
     }
 }

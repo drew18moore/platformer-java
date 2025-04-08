1e5f7a67efed5dfe66c205725b34ec786d713ea8
Drew <dcm752052@gmail.com>
2025-04-08 02:16:36 -0400

    New coin sprite with animation. Coin now scaled.

--- src/main/java/com/drewm/objects/Coin.java
+++ src/main/java/com/drewm/objects/Coin.java
@@ -12,7 +12,9 @@ import java.io.IOException;
 public class Coin {
     private final Playing playing;
     public float worldX, worldY;
-    private BufferedImage sprite;
+    private BufferedImage[] spriteFrames;
+    public int spriteCounter = 0;
+    public int spriteNum = 0;
 
     public Coin(float x, float y, Playing playing) {
         this.playing = playing;
@@ -20,7 +22,17 @@ public class Coin {
         this.worldY = y;
 
         try {
-            this.sprite = ImageIO.read(getClass().getResourceAsStream("/sprites/coin.png"));
+            BufferedImage spriteSheet = ImageIO.read(getClass().getResourceAsStream("/sprites/coin.png"));
+
+            this.spriteFrames = new BufferedImage[4];
+            for (int i = 0; i < spriteFrames.length; i++) {
+                spriteFrames[i] = spriteSheet.getSubimage(
+                        i * Constants.TILE_WIDTH,
+                        0,
+                        Constants.TILE_WIDTH,
+                        Constants.TILE_WIDTH
+                );
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -30,6 +42,17 @@ public class Coin {
         if (getScreenBounds().intersects(playing.player.getBounds())) {
             playing.player.earnCoins(1);
             return true;
+        }
+
+        System.out.println(spriteCounter);
+        spriteCounter++;
+        if (spriteCounter > 24) {
+            if (spriteNum >= 3) {
+                spriteNum = 0;
+            } else {
+                spriteNum++;
+            }
+            spriteCounter = 0;
         }
         return false;
     }
@@ -41,13 +64,13 @@ public class Coin {
                 screenX < Constants.SCREEN_WIDTH &&
                 screenY + Constants.TILE_SIZE > 0 &&
                 screenY < Constants.SCREEN_HEIGHT) {
-            g2.drawImage(sprite, (int) screenX, (int) screenY, sprite.getWidth() * 2, sprite.getHeight() * 2, null);
+            g2.drawImage(spriteFrames[spriteNum], (int) screenX, (int) screenY, spriteFrames[0].getWidth() * Constants.SCALE, spriteFrames[0].getHeight() * Constants.SCALE, null);
         }
     }
 
     public Rectangle2D.Float getScreenBounds() {
         float screenX = this.worldX - playing.player.worldX + playing.player.screenX;
         float screenY = this.worldY - playing.player.worldY + playing.player.screenY;
-        return new Rectangle2D.Float(screenX, screenY, sprite.getWidth() * 2, sprite.getHeight() * 2);
+        return new Rectangle2D.Float(screenX, screenY, spriteFrames[0].getWidth() * Constants.SCALE, spriteFrames[0].getHeight() * Constants.SCALE);
     }
 }
